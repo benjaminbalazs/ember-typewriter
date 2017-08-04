d6d643a5276ac6a65242ed446b013b6bb1c74398
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-08-04 19:12:33 +0400

    fastboot

--- addon/components/ember-typewriter/component.js
+++ addon/components/ember-typewriter/component.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
-import $ from 'jquery';
 import InViewportMixin from 'ember-in-viewport';
 
 export default Ember.Component.extend(InViewportMixin,{
+
   didEnterViewport() {
-    $('.' + this.get('class')).typed({
-      strings: this.get('strings'),
-      contentType: this.get('contentType') //'html' or 'text'
-    });
+      this.$().typed({
+          strings: this.get('strings'),
+          backDelay: this.get('backDelay'),
+          typeSpeed: this.get('typeSpeed'),
+          backSpeed: this.get('backSpeed'),
+          loop: this.get('loop'),
+          contentType: this.get('contentType')
+      });
   }
+  
 });
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+var FastbootTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'ember-typewriter',
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-  },
-  options: {
-    nodeAssets: {
-      'typed.js': function() {
-        return {
-          enabled: !process.env.EMBER_CLI_FASTBOOT,
-          srcDir: 'dist',
-          import: [
-            'typed.min.js'
-          ]
-        };
-      }
+
+    name: 'ember-typewriter',
+
+    included: function(app) {
+
+        this._super.included.apply(this, arguments);
+
+        app.import(this.treePaths.vendor + '/typed.js/typed.min.js');
+
+    },
+
+    treeForVendor: function(vendorTree) {
+
+        var trees = [];
+
+        var typedTree = FastbootTransform(new Funnel(path.join(path.dirname(require.resolve('typed.js'))), {
+            destDir: 'typed.js',
+            include: ['typed.min.js']
+        }));
+
+        if ( vendorTree !== undefined ) { trees.push(vendorTree); }
+        trees.push(typedTree);
+
+        return new MergeTrees(trees);
+
+    },
+
+    isDevelopingAddon: function() {
+        return true;
     }
-  }
+
 };
